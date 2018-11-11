19a3c31c6a6cbacb19671044b2fca0e5cd2195bd
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-11 22:09:08 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,13 +7,13 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    # BRANDS << brand 
-    # BRANDS.uniq!
+    BRANDS << brand 
+    BRANDS.uniq!
     
-  if BRANDS.include?(brand)
-    else
-      BRANDS << brand
-    end 
+  # if BRANDS.include?(brand)
+  #   else
+  #     BRANDS << brand
+  #   end 
   end
 
   def cobble
